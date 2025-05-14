c25425f182d3b4fd5f8f7f023115ab266f0626e8
DejaVolf <87902114+bunnyhop-dev@users.noreply.github.com>
2025-05-15 03:31:30 +0700

    Add files via upload

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+weather=$(curl -s 'wttr.in/Bangkok?format=%C')
+temp=$(curl -s 'wttr.in/Bangkok?format=%t')
+
+case "$weather" in
+  "Sunny") icon="☀️";;
+  "Clear") icon="🌙";;
+  "Partly cloudy") icon="⛅";;
+  "Cloudy") icon="☁️";;
+  "Rain"|"Light rain") icon="🌧️";;
+  "Snow") icon="❄️";;
+  *) icon="🌈";;
+esac
+
+echo "$icon $temp"
